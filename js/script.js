72982911a523755409b849cc240ce18eee9f79f2
RiccardoNestola <riccardinux@gmail.com>
2022-11-30 16:09:23 +0100

    add for

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+const gridContainer = document.querySelector("div.grid");
+console.log(gridContainer);
+
+
+
+for (let i = 1; i <=100; i++) {
+
+    console.log(i);
+    
+    }
+
+
+
+for (let i = 1; i <= 100; i++) {
+
+    const newSquare = document.createElement("div");
+
+    newSquare.classList.add("square");
+
+    gridContainer.appendChild(newSquare);
+    
+}
+
+
